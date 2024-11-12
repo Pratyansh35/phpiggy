2775a709158f091113bbff7e8d50583c3554b71f
Pratyansh Maddhesia <pratyansh35@gmail.com>
2024-11-13 00:17:38 +0530

    production v1

--- cli.php
+++ cli.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/src/Framework/Database.php';
 
+use Dotenv\Dotenv;
 use Framework\Database;
 
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Initialize Database connection
 $db = new Database($_ENV['DB_DRIVER'], [
     'host' => $_ENV['DB_HOST'],
     'port' => $_ENV['DB_PORT'],
