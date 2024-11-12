abc4aca96e550e1d70d6535e61b529f33460d41e
Pratyansh Maddhesia <pratyansh35@gmail.com>
2024-11-13 01:57:43 +0530

    production v4

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
 include __DIR__ . "/../src/App/functions.php";
+require_once __DIR__ . '/../vendor/autoload.php';
+
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
